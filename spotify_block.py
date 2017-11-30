e88a23b8ac84861b6e2b19dd56b0e1579e216bfa
Tyler Lugger <tlugger@n.io>
2017-11-29 20:44:28 -0700

    :sparkles: :construction: Its a block

--- spotify_block.py
+++ spotify_block.py
@@ -1,12 +1,21 @@
 from nio.block.base import Block
-from nio.properties import VersionProperty
+from nio.properties import VersionProperty, StringProperty
+from nio.block.mixins import EnrichSignals
+import spotipy.util as util
 
 
-class Spotify(Block):
+class Spotify(Block, EnrichSignals):
 
     version = VersionProperty('0.1.0')
+    client_id = StringProperty(title='Client ID', default='[[CLIENT_ID]]')
+    client_secret = StringProperty(title='Client Secret', default='[[CLIENT_SECRET]]')
+    scope = StringProperty(title='Scope', default=None)
+    redirect_uri = StringProperty(title='Redirect URI', default='http://localhost:8888/callback')
+    username = StringProperty(title='Username', default='')
 
     def process_signals(self, signals):
+        out_sigs = []
         for signal in signals:
-            pass
-        self.notify_signals(signals)
+            token = {"token": util.prompt_for_user_token(self.username(), self.scope(), self.client_id(), self.client_secret(), self.redirect_uri()) }
+            out_sigs.append(self.get_output_signal(token, signal))
+        self.notify_signals(out_sigs)
