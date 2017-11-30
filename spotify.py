9c5fb1111c945cf01419da77301461e586d4190e
Tyler Lugger <tlugger@n.io>
2017-11-29 21:44:22 -0700

    :sparkels: Added helper function to run first before using the block

---
+++ spotify.py
@@ -0,0 +1,14 @@
+import spotipy.util as util
+
+scope = '[[ YOUR_SCOPE]]'
+username = '[[ YOUR_USERNAME ]]'
+client_id = '[[ YOUR_CLIENT_ID ]]'
+client_secret = '[[ YOUR_CLIENT_SECRET ]]'
+redirect_uri = 'http://localhost:8888/callback'
+
+token = util.prompt_for_user_token(username, scope, client_id, client_secret, redirect_uri)
+
+if token:
+    print("Success")
+else:
+    print("Failed")
